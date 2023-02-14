22007194fb9f8d40a82373bbc78537df528404a4
Valentino Traverso <vt20032003@live.com.ar>
2023-02-14 11:46:19 +0100

    feat{add: get controller to routes}

--- routes/routes.php
+++ routes/routes.php
@@ -2,4 +2,17 @@
 $routesArray = explode("/", $_SERVER['REQUEST_URI']);
 $indexPathAPI = count(array_filter($routesArray)) - DEEP_PROJECT;
 
-print_r($indexPathAPI);
+if($indexPathAPI === 0){
+    $response = array(
+        'status' => 404,
+        'result' => 'Not Found'
+    );
+
+    echo json_encode($response);
+}else if($indexPathAPI === 1 && isset($_SERVER['REQUEST_METHOD'])){
+    switch($_SERVER['REQUEST_METHOD']){
+        case 'GET':
+            require_once BASE_PATH.'/controllerts/getController.php';
+            return;
+    }
+}
